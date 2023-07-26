856e582b60ef4a55368933acf591986b2a7dd8d6
MOONcitizenX <mooncitizenx@gmail.com>
2023-07-26 15:06:45 +0300

    feat: add common button with circle component

---
+++ components/common/ButtonCircle/ButtonCircle.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+interface Props {
+  type: 'link' | 'button';
+  to?: string;
+  text: string;
+  onClick?: () => void;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="button-container">
+    {{ props.text }}
+    <nuxt-link v-if="props.type === 'link'" :to="props.to">
+      <div class="arrow-container">
+        <img src="/_nuxt/assets/images/icons/linkButton.svg" alt="" />
+      </div>
+    </nuxt-link>
+
+    <button v-else class="button-container" @click="props.onClick">
+      <div class="arrow-container">
+        <img src="/_nuxt/assets/images/icons/linkButton.svg" alt="" />
+      </div>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import 'ButtonCircle.scss';
+</style>
