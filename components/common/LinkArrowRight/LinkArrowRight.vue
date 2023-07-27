3b8d72a57dde37b6a57ecf23f484ca9f99fa51b5
MOONcitizenX <mooncitizenx@gmail.com>
2023-07-27 15:54:39 +0300

    fix: fix svg components

--- components/common/LinkArrowRight/LinkArrowRight.vue
+++ components/common/LinkArrowRight/LinkArrowRight.vue
@@ -1,3 +1,46 @@
+<template>
+  <svg
+    :width="props.width ?? 20"
+    :height="props.height ?? 20"
+    :viewBox="`0 0 ${props.width ?? 20} ${props.height ?? 20}`"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_674_735)">
+      <path
+        d="M4.16699 10H15.8337"
+        :stroke="props.color ?? '#111111'"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.5 13.3333L15.8333 10"
+        :stroke="props.color ?? '#111111'"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.5 6.6665L15.8333 9.99984"
+        :stroke="props.color ?? '#111111'"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_674_735">
+        <rect
+          :width="props.width ?? 20"
+          :height="props.height ?? 20"
+          fill="white"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
+
 <script lang="ts" setup>
 interface Props {
   color?: string;
@@ -7,40 +50,3 @@ interface Props {
 
 const props = defineProps<Props>();
 </script>
-
-<template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    :width="props.width ?? 20"
-    :height="props.height ?? 20"
-    :viewBox="`0 0 ${props.width ?? 20} ${props.height ?? 20}`"
-    fill="none"
-  >
-    <g clip-path="url(#clip0_808_226)">
-      <path
-        d="M5.83301 14.1665L14.1663 5.83317"
-        :stroke="props.color ?? '#111111'"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <path
-        d="M14.167 13.3335V5.8335H6.66699"
-        :stroke="props.color ?? '#111111'"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_808_226">
-        <rect
-          :width="props.width ?? 20"
-          :height="props.height ?? 20"
-          fill="white"
-          transform="matrix(1 0 0 -1 0 20)"
-        />
-      </clipPath>
-    </defs>
-  </svg>
-</template>
