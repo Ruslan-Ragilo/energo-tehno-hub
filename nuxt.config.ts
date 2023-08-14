3f6bb6de5d3a78c350285cf45cfc1ae2aa6d1880
MOONcitizenX <mooncitizenx@gmail.com>
2023-08-14 11:28:23 +0300

    feat: add favicon for all devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,29 @@ export default defineNuxtConfig({
       link: [
         { href: "https://unpkg.com/aos@2.3.1/dist/aos.css", rel: "stylesheet" },
         { rel: "icon", type: "image/png", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+      ],
+      meta: [
+        { name: "msapplication-TileColor", content: "#2b5797" },
+        { name: "theme-color", content: "#ffffff" },
       ],
       script: [{ src: "https://unpkg.com/aos@2.3.1/dist/aos.js", body: true }],
     },
