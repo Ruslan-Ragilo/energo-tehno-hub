55262bc505274f8f4a591202d6f005bb006e6774
MOONcitizenX <mooncitizenx@gmail.com>
2023-08-22 16:40:28 +0300

    fix: header burger button size

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -2,6 +2,10 @@
 const isOpenMobMenu = ref(false);
 const textForDropdown = ref("");
 
+const toggleIsOpenMobMenu = () => {
+  isOpenMobMenu.value = !isOpenMobMenu.value;
+};
+
 const handleDropdown = (text) => {
   textForDropdown.value = text;
 };
@@ -34,7 +38,7 @@ const dataLinkDropdown = {
           <img src="/images/logo.svg" alt="logo" />
         </div>
       </nuxt-link>
-      <nav :class="{ ['header-nav']: true}">
+      <nav :class="{ ['header-nav']: true }">
         <div
           class="link-dropdown"
           @mouseover="handleDropdown('Экосистема')"
@@ -113,7 +117,7 @@ const dataLinkDropdown = {
         </div>
       </nav>
 
-      <div :class="['mob-nav', {['active']: isOpenMobMenu}]">
+      <div :class="['mob-nav', { ['active']: isOpenMobMenu }]">
         <FooterColumn
           heading="Экосистема"
           :links="[
@@ -138,7 +142,7 @@ const dataLinkDropdown = {
             { text: 'Контакты', href: 'more' },
           ]"
         />
-      </div> 
+      </div>
 
       <div class="header-right">
         <!-- <CommonLangSwitcher /> -->
@@ -149,18 +153,57 @@ const dataLinkDropdown = {
         />
       </div>
 
-      <button class="burger" @click="isOpenMobMenu = !isOpenMobMenu">
-        <svg class="hb" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="#333" stroke-width=".6" fill="rgba(0,0,0,0)" stroke-linecap="round" style="cursor: pointer">
+      <button class="burger" @click="toggleIsOpenMobMenu">
+        <svg
+          class="hb"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 10 10"
+          stroke="#333"
+          stroke-width=".6"
+          fill="rgba(0,0,0,0)"
+          stroke-linecap="round"
+          style="cursor: pointer"
+        >
           <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
-            <animate dur="0.2s" attributeName="d" values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7" fill="freeze" begin="start.begin" />
-            <animate dur="0.2s" attributeName="d" values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7" fill="freeze" begin="reverse.begin" />
+            <animate
+              dur="0.2s"
+              attributeName="d"
+              values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7"
+              fill="freeze"
+              begin="start.begin"
+            />
+            <animate
+              dur="0.2s"
+              attributeName="d"
+              values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7"
+              fill="freeze"
+              begin="reverse.begin"
+            />
           </path>
           <rect width="8" height="8" stroke="none">
-            <animate dur="2s" id="reverse" attributeName="width" begin="click" />
+            <animate
+              dur="2s"
+              id="reverse"
+              attributeName="width"
+              begin="click"
+            />
           </rect>
           <rect width="8" height="8" stroke="none">
-            <animate dur="0.001s" id="start" attributeName="width" values="10;0" fill="freeze" begin="click" />
-            <animate dur="0.001s" attributeName="width" values="0;10" fill="freeze" begin="reverse.begin" />
+            <animate
+              dur="0.001s"
+              id="start"
+              attributeName="width"
+              values="10;0"
+              fill="freeze"
+              begin="click"
+            />
+            <animate
+              dur="0.001s"
+              attributeName="width"
+              values="0;10"
+              fill="freeze"
+              begin="reverse.begin"
+            />
           </rect>
         </svg>
       </button>
