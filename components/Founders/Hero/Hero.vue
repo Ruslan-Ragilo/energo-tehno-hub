beb10b2060a8f0de1727d9e2fff2f7915c7a562a
MOONcitizenX <mooncitizenx@gmail.com>
2023-09-28 00:54:13 +0300

    feat: add Founders Hero block

---
+++ components/Founders/Hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-wrapper">
+    <h1 class="founders-title">
+      Познакомьтесь с фаундерами «Энерготехнохаба Петербург»
+    </h1>
+    <div class="right-block">
+      <CommonText
+        text="Здесь предприниматели со всей России, которые выбрали делать технологический бизнес в «Энерготехнохабе Петербург»."
+        size="m"
+        theme="gray"
+        weight="regular"
+        className="right-text"
+      ></CommonText>
+      <CommonButtonCircle type="link" text="Узнать больше" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+@import "./Hero.scss";
+</style>
--- pages/founders/index.vue
+++ pages/founders/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <section class="hero-wrapper">
+      <FoundersHero />
+      <FoundersVideoSlider />
+    </section>
+  </div>
 </template>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
